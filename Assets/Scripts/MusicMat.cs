1f287b795df25bd5158c8a1651325a46f663f56d
fumikomi <56688085+fumikomi@users.noreply.github.com>
2020-08-04 19:38:11 +0300

    Quick change for the Music Glove interface
    Changed the layout for the UI from the Music Mat to the Music Glove. Had to make two different backgrounds for the UI buttons, and quickly implemented a fix to make both of them backgrounds pulse and change color.

--- Assets/Scripts/MusicMat.cs
+++ Assets/Scripts/MusicMat.cs
@@ -1,42 +1,53 @@
 ﻿using UnityEngine;
 
-public class MusicMat : MonoBehaviour
-{
-  public bool flashOnBeat = true;
+public class MusicMat : MonoBehaviour {
+    public bool flashOnBeat = false;
 
-  private Color defaultColor;
-  private Material material;
-  private Transform childTransform;
-  private Vector3 defaultPosition;
-  private float beatDisplacement = .1f;
-  private Clock clock;
+    private Color defaultColor;
+    private Material material_left;
+    private Material material_right;
+    private Transform childTransform_left;
+    private Transform childTransform_right;
+    private Vector3 defaultPosition_left;
+    private Vector3 defaultPosition_right;
+    private float beatDisplacement = .1f;
+    private Clock clock;
 
-  void Awake()
-  {
-    childTransform = transform.GetChild(0).transform;
-    defaultPosition = childTransform.localPosition;
-    material = GetComponentInChildren<Renderer>().material;
-    defaultColor = material.color;
-    clock = FindObjectOfType<Clock>();
-  }
 
-  void Update()
-  {
-    var time = Time.deltaTime * (1 / clock.HalfInterval);
-    material.color = Color.Lerp(material.color, defaultColor, time);
-    childTransform.localPosition = Vector3.Lerp(childTransform.localPosition, defaultPosition, time);
-  }
+    /*
+        Made a quick work-around so that there are two different backgrounds that pulse and change color instead of one.
+        This is a poor implementation, and should be fixed in the future!
+    */
+    //TODO: Fix the poor code for the two backgrounds!
+    void Awake() {
+        childTransform_left = transform.GetChild(0).transform;
+        childTransform_right = transform.GetChild(1).transform;
+        defaultPosition_left = childTransform_left.localPosition;
+        defaultPosition_right = childTransform_right.localPosition;
+        material_left = childTransform_left.GetComponent<Renderer>().material;
+        material_right = childTransform_right.GetComponent<Renderer>().material;
+        defaultColor = material_left.color;
+        clock = FindObjectOfType<Clock>();
+    }
 
-  void OnEnable()
-  {
-    var constants = FindObjectOfType<Constants>();
-    Clock.OnBeat += delegate { Flash(constants.onbeatColor); };
-    Clock.DownBeat += delegate { Flash(constants.downbeatColor); };
-  }
+    void Update() {
+        var time = Time.deltaTime * (1 / clock.HalfInterval);
+        material_left.color = Color.Lerp(material_left.color, defaultColor, time);
+        material_right.color = Color.Lerp(material_right.color, defaultColor, time);
+        childTransform_left.localPosition = Vector3.Lerp(childTransform_left.localPosition, defaultPosition_left, time);
+        childTransform_right.localPosition = Vector3.Lerp(childTransform_right.localPosition, defaultPosition_right, time);
+    }
 
-  private void Flash(Color color)
-  {
-    material.color = color;
-    childTransform.localPosition = defaultPosition + new Vector3(0, 0, 1f) * beatDisplacement;
-  }
+    void OnEnable() {
+        var constants = FindObjectOfType<Constants>();
+        Clock.OnBeat += delegate { Flash(constants.onbeatColor); };
+        Clock.DownBeat += delegate { Flash(constants.downbeatColor); };
+    }
+
+    private void Flash(Color color) {
+        material_left.color = color;
+        material_right.color = color;
+        childTransform_left.localPosition = defaultPosition_left + new Vector3(0, 0, 1f) * beatDisplacement;
+        childTransform_right.localPosition = defaultPosition_right + new Vector3(0, 0, 1f) * beatDisplacement;
+    }
 }
